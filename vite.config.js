6faac12066a5dddf196955a096667a3d0828b5e8
Abay Serikov <abay.s@scopicsoftware.com>
2026-03-06 20:17:10 +0600

    fix: api proxy paths for vercel

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/replicate-api': {
+      '/api/replicate-api': {
         target: 'https://api.replicate.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/replicate-api/, ''),
+        rewrite: (path) => path.replace(/^\/api\/replicate-api/, ''),
       },
-      '/elevenlabs-api': {
+      '/api/elevenlabs-api': {
         target: 'https://api.elevenlabs.io',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/elevenlabs-api/, ''),
+        rewrite: (path) => path.replace(/^\/api\/elevenlabs-api/, ''),
       }
     }
   },
